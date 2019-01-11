c311baebefadd48a9d24af978a1c18ccf67eede5
Stephen Hwang <stvhwang@yahoo.com>
2019-01-10 18:40:34 -0800

    adding missing points columns

--- Joins/POIRPlayers_InnerJoin.sql
+++ Joins/POIRPlayers_InnerJoin.sql
@@ -2,7 +2,7 @@
 --Joins the POIRplayers from the two Memphis game tables
 --Finds POIRplayers who are common to both games
 
-select game1.player_name
+select game1.player_name, game1.player_points, game2.player_points
 from da_nba.poirplayers_game0021600009 game1
 join da_nba.poirplayers_game0021600039 game2
 on game1.player_name = game2.player_name
